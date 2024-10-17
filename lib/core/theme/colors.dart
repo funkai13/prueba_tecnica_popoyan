2e606f02b0fb6a3fc4d30e04a3f00f910c2a338b
funkai13 <evinssonmedrano@gmail.com>
2024-10-17 17:30:05 -0600

    Create colors.dart

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Colors.blueGrey;
+  static const Color secondaryColor = Colors.teal;
+  static const Color backgroundColor = Color(0xFFFAFAFA);
+  static const Color cardColor = Colors.white;
+  static const Color dividerColor = Colors.grey;
+  static const Color textColor = Colors.black87;
+}
