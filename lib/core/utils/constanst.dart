87309a31d22093d3d06245ccea58f29f8451eca9
funkai13 <evinssonmedrano@gmail.com>
2024-10-17 17:30:19 -0600

    Update constanst.dart

--- lib/core/utils/constanst.dart
+++ lib/core/utils/constanst.dart
@@ -3,15 +3,3 @@ class Breakpoints {
   static const double tablet = 1024;
   static const double desktop = 1440;
 }
-
-class TextSizes {
-  // Tamaños para móvil
-  static const double mobileTitle = 20;
-  static const double mobileBody = 12.0;
-  static const double mobileSubtitle = 16.0;
-
-  // Tamaños para tabletas
-  static const double tabletTitle = 25;
-  static const double tabletBody = 16.0;
-  static const double tabletSubtitle = 18.0;
-}
