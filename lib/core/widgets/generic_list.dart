38b7034754f77dc939efa3993bffae0e947195e1
funkai13 <evinssonmedrano@gmail.com>
2024-10-18 11:25:41 -0600

    Update generic_list.dart

--- lib/core/widgets/generic_list.dart
+++ lib/core/widgets/generic_list.dart
@@ -3,19 +3,17 @@ import 'package:flutter/material.dart';
 class GenericList<T> extends StatelessWidget {
   final List<T> items;
   final Widget Function(T item) itemBuilder;
-  final EdgeInsets padding;
 
   const GenericList({
     super.key,
     required this.items,
     required this.itemBuilder,
-    this.padding = const EdgeInsets.all(12),
   });
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      padding: padding,
+      padding: const EdgeInsets.all(12),
       itemCount: items.length,
       itemBuilder: (context, index) {
         final item = items[index];
